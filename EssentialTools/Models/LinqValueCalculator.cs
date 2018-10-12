8799f34a60d8da12e5a5a83dc4705edb869f3817
sajeewa <saj@saj.com>
2018-10-12 19:52:46 +0100

    Initial commit

---
+++ EssentialTools/Models/LinqValueCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EssentialTools.Models
+{
+    //note we don't need to add a reference to Poructs because we are within the same namespace.
+    public class LinqValueCalculator : IValueCalculator {
+        public decimal ValueProducts(IEnumerable<Product> products)
+        {
+            return products.Sum(p => p.Price); //note "Sum" is a linq extension method
+                                                 
+        }
+    }
+}
